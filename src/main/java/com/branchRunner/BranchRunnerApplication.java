3072e81fd649d30ebd69077b0085af53dcc696ac
esteban2505J <juane.ramirezt@uqvirtual.edu.co>
2025-12-03 15:19:39 -0500

    Initial commit from local.

---
+++ src/main/java/com/branchRunner/BranchRunnerApplication.java
@@ -0,0 +1,13 @@
+package com.branchRunner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BranchRunnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BranchRunnerApplication.class, args);
+	}
+
+}
